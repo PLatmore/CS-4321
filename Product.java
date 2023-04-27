9a38cb42d8eb6f6809699943afff6ba66e97715f
PLatmore <131789300+PLatmore@users.noreply.github.com>
2023-04-27 14:34:00 -0400

    Add files via upload

--- Product.java
+++ Product.java
@@ -1,66 +1,53 @@
+package CommandLineInterface;
+
 public class Product {
-    private int id;
+    private String productId;
     private String name;
     private String manufacturer;
     private double price;
     private int quantity;
-    private boolean isFood;
+    private boolean isFoodItem;
 
-    public Product(int id, String name, String manufacturer, double price, int quantity, boolean isFood) {
-        this.id = id;
+    public Product(String productId, String name, String manufacturer, double price, int quantity, boolean isFoodItem) {
+        this.productId = productId;
         this.name = name;
         this.manufacturer = manufacturer;
         this.price = price;
         this.quantity = quantity;
-        this.isFood = isFood;
+        this.isFoodItem = isFoodItem;
     }
 
-    // getters and setters for all fields
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public String getProductId() {
+        return productId;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getManufacturer() {
         return manufacturer;
-    }
-
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public boolean isFoodItem() {
+        return isFoodItem;
     }
 
-    public boolean isFood() {
-        return isFood;
+    public double getTotalPrice() {
+        return price * quantity;
     }
 
-    public void setFood(boolean isFood) {
-        this.isFood = isFood;
+    @Override
+    public String toString() {
+        String foodItemStr = isFoodItem ? "Yes" : "No";
+        return String.format("%-10s%-20s%-20s$%-10.2f%-10d$%-20.2f%-10s", productId, name, manufacturer, price, quantity, getTotalPrice(), foodItemStr);
     }
 }
